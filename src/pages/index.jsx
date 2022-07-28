8c85c75682d36e25aa932925f0943cdcb933692b
Rafael Tivane <luislangabusiness@gmail.com>
2022-07-28 14:29:05 +0200

    create handlers

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,3 +1,4 @@
+import { useCallback, useEffect, useRef } from "react"
 import { Button, IconButton, Typography } from "@mui/material";
 import classNames from "classnames";
 
@@ -5,27 +6,50 @@ import classes from "src/styles/home.module.css";
 import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
 import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 
-import { useCallback, useEffect, useRef } from "react"
-
 import Link from "src/components/link";
 
 const Home = () => {
     const listRef = useRef(null);
-    const index = useRef(null);
+    const children = useRef([]);
+    const index = useRef(0);
+
 
     const setWidth = useCallback(() => {
-        const children = [ ...listRef.current.children ];
         const width = window.innerWidth;
 
-        children.forEach(item => {
+        children.current.forEach((item, index) => {
             item.style.width = `${width}px`;
+            item.style.left = `${width * index}px`
         });
 
-        listRef.current.style.width = `${width * children.length}px`;
+        listRef.current.style.width = `${width * children.current.length}px`;
     }, []);
 
+    const slide = useCallback(() => {
+        console.log(index.current);
+        listRef.current.style.transform = `translateX(-${window.innerWidth * index.current}px)`;
+    }, []);
+
+    const previousHandler = useCallback(() => {
+        if((index.current - 1) >= 0) {
+            index.current = index.current - 1;
+        } 
+
+        slide();
+    }, [ slide ]);
+
+    const nextHandler = useCallback(() => {
+        if((index.current + 1) < children.current.length) {
+            index.current = index.current + 1;
+        } 
+        
+        slide();
+    }, [ slide ]);
+
+
     useEffect(() => {
-        setWidth()
+        children.current = [ ...listRef.current.children ];
+        setWidth();
         window.addEventListener("resize", setWidth);
 
         const currentWindow = window;
@@ -39,11 +63,11 @@ const Home = () => {
         <main>
             <section className="overflow-hidden relative w-full">
                 <ul 
-                    className="list flex"
+                    className={classNames(classes.list, "relative")}
                     ref={listRef}>
-                    <li className="">
+                    <li className="absolute h-full">
                         <div className={classNames(classes.hero, classes.secureHero, `bg-no-repeat bg-center bg-cover flex flex-col 
-                            items-center justify-center`)}>
+                            h-full items-center justify-center`)}>
                             <Typography
                                 className={classNames(classes.heroTitle, `font-bold leading-8 text-center text-2xl sm:text-4xl md:text-5xl lg:text-6xl text-white uppercase`)}
                                 component="h1">
@@ -58,9 +82,9 @@ const Home = () => {
                             </Link>
                         </div>
                     </li>
-                    <li className="w-full">
+                    <li className="absolute h-full">
                         <div className={classNames(classes.hero, classes.exchangeHero, `bg-no-repeat bg-center bg-cover flex flex-col 
-                            items-center justify-center`)}>
+                            h-full items-center justify-center`)}>
                             <Typography
                                 className={classNames(classes.heroTitle, `font-bold leading-8 text-center text-2xl sm:text-4xl md:text-5xl lg:text-6xl text-white uppercase`)}
                                 component="h1">
@@ -77,10 +101,14 @@ const Home = () => {
                     </li>
                 </ul>
                 <div className={classNames(classes.heroButtons, 'absolute flex justify-between px-5 sm:px-4 w-full')}>
-                    <IconButton className="text-amber-500">
+                    <IconButton 
+                        className="text-amber-500"
+                        onClick={previousHandler}>
                         <ArrowBackIosIcon className="text-4xl" />
                     </IconButton>
-                    <IconButton className="text-amber-500">
+                    <IconButton 
+                        className="text-amber-500"
+                        onClick={nextHandler}>
                         <ArrowForwardIosIcon  className="text-4xl" />
                     </IconButton>
                 </div>
