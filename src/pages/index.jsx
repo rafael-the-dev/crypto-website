126549337b2be5d759462052d5a9240f5fe93300
Rafael Tivane <luislangabusiness@gmail.com>
2022-07-28 13:27:42 +0200

    finish hero responsiveness

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -10,18 +10,18 @@ const Home = () => {
     return (
         <main>
             <section>
-                <div className={classNames(classes.secureHero, `bg-no-repeat bg-center bg-cover flex flex-col 
+                <div className={classNames(classes.hero, classes.secureHero, `bg-no-repeat bg-center bg-cover flex flex-col 
                     items-center justify-center`)}>
                     <Typography
-                        className={classNames(`text-center text-2xl text-white uppercase`)}
+                        className={classNames(classes.heroTitle, `font-bold leading-8 text-center text-2xl sm:text-4xl md:text-5xl lg:text-6xl text-white uppercase`)}
                         component="h1">
                         <span className="text-amber-600">Secure</span> and <span className="text-amber-600">Easy way </span>
                         <br/>to bitcoin
                     </Typography>
                     <Link className="mt-4" href="/">
                         <Button 
-                            className="border-amber-600 text-amber-700 hover:bg-amber-600 
-                            hover:border-amber-600 hover:text-white"
+                            className="border-amber-500 text-amber-500 hover:bg-amber-600 
+                            hover:border-amber-600 hover:text-white sm:py-2 sm:px-12"
                             variant="outlined">Learn more</Button>
                     </Link>
                 </div>
