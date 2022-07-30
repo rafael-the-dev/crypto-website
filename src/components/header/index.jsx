7f8bb0ae7aa288ca3cea646c9329636a31ccc8d1
Rafael Tivane <luislangabusiness@gmail.com>
2022-07-30 09:07:19 +0200

    add windows on scroll handler

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,4 +1,4 @@
-import { useCallback, useMemo, useState } from "react"
+import { useCallback, useEffect, useMemo, useRef, useState } from "react"
 import { Button, Hidden, IconButton } from "@mui/material";
 import classNames from "classnames";
 import Image from "next/image";
@@ -20,27 +20,66 @@ const Header = () => {
     const [ open, setOpen ] = useState(false);
     const [ openSearch, setOpenSearch ] = useState(false);
 
+    const headerRef = useRef(null);
+    const firstRowRef = useRef(null);
+    const secondRowRef = useRef(null);
+
     const toggleOpen = useCallback(() => setOpen(b => !b), []);
 
     const logo = useMemo(() => <Image alt="logo" src={Logo} />, []);
     const buttons = useMemo(() => <Buttons />, []);
     const navigation = useMemo(() => <Navigation closeHandler={toggleOpen} open={open} />, [ open, toggleOpen ]);
     const bitcoinStats = useMemo(() => <BitcoinStats />, [])
+    
     const cartLink = useMemo(() => (
         <Link className="flex items-center md:ml-4 lg:ml-6" href="/">
             <ShoppingCartIcon  className={classNames(classes.cartIcon, "md:text-white md:hover:text-amber-700")} />
         </Link>
     ), []);
+
     const searchButton = useMemo(() => (
         <IconButton className="md:p-0 md:ml-4 lg:ml-6">
             { openSearch ? <CloseIcon className="text-red-700 hover:text-red-500" /> : <SearchIcon className="text-white hover:text-amber-700" /> }
         </IconButton>
     ), [ openSearch ]);
+    
+    const hasClass = useCallback(({ element, classname }) => element.classList.contains(classname), []);
+
+    const scrollHandler = useCallback(() => {
+        if(window.scrollY > 80) {
+            if(!hasClass({ element: headerRef.current, classname: classes.headerFixed }))
+                headerRef.current.classList.add(classes.headerFixed);
+        } else {
+            if(headerRef.current.classList.contains(classes.headerFixed))
+                headerRef.current.classList.remove(classes.headerFixed);
+        }
+
+        if(window.innerWidth >= 768) {
+            if(!hasClass({ element: firstRowRef.current, classname: "hidden" }))
+                firstRowRef.current.classList.add("hidden");
+
+            if(hasClass({ element: secondRowRef.current, classname: "hidden" }))
+                secondRowRef.current.classList.remove("hidden");
+        } else {
+            if(hasClass({ element: firstRowRef.current, classname: "hidden" }))
+                firstRowRef.current.classList.remove("hidden");
+
+            if(!hasClass({ element: secondRowRef.current, classname: "hidden" }))
+                secondRowRef.current.classList.add("hidden");
+        }
+    }, [])
+
+    useEffect(() => {
+        window.addEventListener("scroll", scrollHandler);
+    }, [ scrollHandler ]);
 
     return (
-        <header>
+        <header 
+            className={classNames(classes.header)}
+            ref={headerRef}>
             <div className="bg-neutral-900 flex items-center justify-between py-3 px-5 md:bg-neutral-800
-                md:flex-col md:py-6 lg:flex-row">
+                md:flex-col md:py-6 lg:flex-row"
+                ref={firstRowRef}>
                 { logo }
                 <Hidden mdUp>
                     <IconButton 
@@ -58,7 +97,8 @@ const Header = () => {
                 { navigation }
             </Hidden>
             <div className="bg-neutral-800 flex justify-between py-2 px-5 md:justify-center md:bg-neutral-900
-                sm:flex-col sm:items-center sm:py-6 md:flex-row md:py-2">
+                sm:flex-col sm:items-center sm:py-6 md:flex-row md:py-2"
+                ref={secondRowRef}>
                 <Hidden mdDown>{ navigation }</Hidden>
                 <Hidden mdDown>{ cartLink }</Hidden>
                 <Hidden mdDown>{searchButton}</Hidden>
