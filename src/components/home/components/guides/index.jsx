4d66ed9991ce33023ca3edd3391a66d8af5e823c
Rafael Tivane <luislangabusiness@gmail.com>
2022-08-02 08:22:21 +0200

    add icons to guides' card

--- src/components/home/components/guides/index.jsx
+++ src/components/home/components/guides/index.jsx
@@ -2,20 +2,25 @@ import classNames from "classnames";
 
 import Card from "./components/card";
 
+import classes from "./styles.module.css";
+
 const GuidesContainer = () => (
     <div className="bg-neutral-900 px-5">
         <div className={classNames("bg-neutral-800 py-8 px-3")}>
             <Card 
                 description="Get it on PC or Mobile to create, send and receive bitcoins."
                 title="Download Bitcoin Wallet"
+                titleIcon={classes.download}
             />
             <Card 
                 description="Add bitcoins you've created or exchanged via credit card."
                 title="Add coins to your Wallet"
+                titleIcon={classes.add}
             />
             <Card 
                 description="Enter receiver's address, specify the amount and send."
                 title="Buy/Sell with Wallet"
+                titleIcon={classes.buySell}
             />
         </div>
     </div>
