86ab7597dd02128c74c3c52e70af824e79a1a886
rafael-the-dev <rafaeltivane2000@gmail.com>
2022-08-04 09:16:39 +0200

    create packages state, its components and animations

--- src/components/home/components/packages/index.jsx
+++ src/components/home/components/packages/index.jsx
@@ -1,9 +1,14 @@
-import { Typography } from "@mui/material";
+import { Button, Typography } from "@mui/material";
+import { useCallback, useMemo, useRef, useState } from "react";
 import classNames from "classnames";
 
 import classes from "./styles.module.css"
 
 const PackagesSection = () => {
+    const packagesTypes = useRef({ buy: "BUY", sell: "SELL" })
+    const [ selectedPackage, setSelectedPackage ] = useState(packagesTypes.current.buy);
+
+    const clickHandler = useCallback(prop => () => setSelectedPackage(prop), [])
 
     return (
         <section className="bg-neutral-900 flex flex-col items-center px-5 py-12">
@@ -17,6 +22,22 @@ const PackagesSection = () => {
                 uppercase before:bg-amber-600 after:bg-amber-600 md:flex md:items-center`)}>
                 Purchase Bitcoin using credit card or with your linked bank account.
             </Typography>
+
+            <div className={classNames(classes.buttonsContainer, 
+                "border border-solid border-gray-400 flex items-stretch mt-8 relative",
+                "before:absolute before:bg-amber-600",
+                selectedPackage === packagesTypes.current.buy ? classes.buttonsContainerLeft : classes.buttonsContainerRight)}>
+                <Button 
+                    className={classes.toggleButton}
+                    onClick={clickHandler(packagesTypes.current.buy)}>
+                    Buy
+                </Button>
+                <Button 
+                    className={classes.toggleButton}
+                    onClick={clickHandler(packagesTypes.current.sell)}>
+                    Sell
+                </Button>
+            </div>
         </section>
     );
 };
