3f742219599a6d06c223e20d1f0a5d6326eceb2a
rafael-the-dev <rafaeltivane2000@gmail.com>
2022-08-05 16:51:35 +0200

    finish card animation

--- src/components/home/components/our-experts/components/card/index.jsx
+++ src/components/home/components/our-experts/components/card/index.jsx
@@ -1,17 +1,34 @@
 import Image from "next/image";
-import { Typography } from "@mui/material";
+import { IconButton, Typography } from "@mui/material";
 import classNames from "classnames";
+
+import FacebookIcon from '@mui/icons-material/Facebook';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
 
 import classes from "./styles.module.css"
 
 const Card = ({ image, name, position }) => (
     <li className={classNames(classes.card, "bg-neutral-800 card mb-6")}> 
-        <Image 
-            alt={name}
-            className="object-cover object-center"
-            layout="fill"
-            src={image}
-        />
+        <div className={classNames(classes.imageContainer, "relative")}>
+            <Image 
+                alt={name}
+                className="object-cover object-center"
+                layout="fill"
+                src={image}
+            />
+            <div className="absolute flex items-center justify-center social-media">
+                <IconButton className="text-white mr-3">
+                    <FacebookIcon />
+                </IconButton>
+                <IconButton className="text-white mr-3">
+                    <TwitterIcon />
+                </IconButton>
+                <IconButton className="text-white">
+                    <LinkedInIcon />
+                </IconButton>
+            </div>
+        </div>
         <div className={classNames("info py-6 relative text-center text-white before:bg-amber-600")}>
             <Typography
                 component="h2"
@@ -37,8 +54,23 @@ const Card = ({ image, name, position }) => (
                         width: 100%;
                     }
 
+                    .social-media {
+                        background-color: rgba(0, 0, 0, 0);
+                        height: 0;
+                        left: 50%;
+                        top: 50%;
+                        transform: scale(0) translate(-50%, -50%);
+                        transition: all .6s ease-in;
+                        width: 0;
+                        z-index: 9;
+                    }
+
                     .card:hover .info::before {
                         height: 100%;
+                    }
+
+                    .card:hover .social-media {
+                        transform: scale(1) translate(-50%, -50%);
                     }
                 `
             }
