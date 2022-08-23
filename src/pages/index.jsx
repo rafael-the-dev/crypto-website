e231a229e09489ce30f2f1533bd00354b2569f85
rafael-the-dev <luislangabusiness@gmail.com>
2022-08-23 19:09:39 +0200

    remove unnecessary imports

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,12 +1,3 @@
-import { useCallback, useEffect, useRef } from "react"
-import { Button, IconButton, Typography } from "@mui/material";
-import classNames from "classnames";
-import Image from "next/image"
-
-import classes from "src/styles/home.module.css";
-import aboutImage from "public/about-us.png"
-
-import Link from "src/components/link";
 import Hero from "src/components/home-carousel"
 import AbousUs from "src/components/home/components/about-us";
 import Guides from "src/components/home/components/guides";
